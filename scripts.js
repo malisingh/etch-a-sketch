c10217ca8d7ff5c1de2155437dcb6aa1155f8799
malisingh <114786303+malisingh@users.noreply.github.com>
2024-11-08 16:37:48 -0500

    updated styling and completed step 3 to add the hover effect

--- scripts.js
+++ scripts.js
@@ -7,8 +7,13 @@ function createFlexboxGrid(rows, cols) {
   for (let i = 0; i < rows * cols; i++) {
     const item = document.createElement("div");
     item.classList.add("grid-item");
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "lightcoral";
+    });
     container.appendChild(item);
   }
   document.body.appendChild(container);
 }
 createFlexboxGrid(16, 16);
+
+// document.querySelector("#grid-selector")
