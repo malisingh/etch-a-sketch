0855c14a93e70c429cab61d10dae1bb60d4e5cc3
malisingh <114786303+malisingh@users.noreply.github.com>
2024-11-08 20:12:53 -0500

    completed project, unable to get grid to display evenly

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-function createFlexboxGrid(rows, cols) {
+function createFlexboxGrid(rows, columns) {
   const container = document.createElement("div");
   container.classList.add("grid-container");
   container.style.display = "flex";
   container.style.flexWrap = "wrap";
 
-  for (let i = 0; i < rows * cols; i++) {
+  for (let i = 0; i < rows * columns; i++) {
     const item = document.createElement("div");
     item.classList.add("grid-item");
     item.addEventListener("mouseover", () => {
-      item.style.backgroundColor = "lightcoral";
+      item.style.backgroundColor = getRandomColor();
     });
     container.appendChild(item);
   }
@@ -16,4 +16,49 @@ function createFlexboxGrid(rows, cols) {
 }
 createFlexboxGrid(16, 16);
 
-// document.querySelector("#grid-selector")
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  let gridSize = prompt(
+    "How many squares do you want? Number must be less than 100"
+  );
+  let chosenSize = parseInt(gridSize);
+  if (chosenSize > 100 || chosenSize < 1 || !Number.isInteger(chosenSize)) {
+    alert("Please choose a number between 1 and 100");
+  } else {
+    const oldContainer = document.querySelector(".grid-container");
+    oldContainer.remove();
+    createFlexboxGrid(chosenSize, chosenSize);
+  }
+});
+
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   let gridSize = prompt(
+//     "How many squares do you want? Number must be less than 100"
+//   );
+//   let chosenSize = parseInt(gridSize);
+//   if (chosenSize > 100 || chosenSize < 1 || !Number.isInteger(chosenSize)) {
+//     alert("Please choose a number between 1 and 100");
+//   } else {
+//     const oldContainer = document.querySelector(".grid-container");
+//     oldContainer.remove();
+//     const newContainer = document.querySelector("#canvas");
+//     for (let i = 0; i < chosenSize * chosenSize; i++) {
+//       const newItem = document.createElement("div");
+//       newItem.classList.add("grid-item");
+//       newItem.addEventListener("mouseover", () => {
+//         newItem.style.backgroundColor = "lightcoral";
+//       });
+//       newContainer.appendChild(newItem);
+//     }
+//   }
+// });
